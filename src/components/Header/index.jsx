5a7d8548d4e35515a8924febecb2496f0841e689
Carlos David Madrigal Núñez <davidemadrigal@gmail.com>
2020-02-06 10:33:57 -0600

    Team section finished
    team section finishe

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,129 +4,129 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import { Tabs, Tab, Box, Grid } from '@material-ui/core'
-import { LocationOn, Phone, Email } from '@material-ui/icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCode } from '@fortawesome/free-solid-svg-icons'
+import { LocationOn, Phone, Email, Facebook, Twitter } from '@material-ui/icons'
 
 function NavBar({ value, handleChange }) {
-  return (
-    <Box component='div' className='navbar'>
-      <AppBar position='static' className='headers'>
-        <Toolbar className='header'>
-          <Typography variant='h6' className='header__name'>
-            {APLICATION_NAME}
-          </Typography>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label='simple tabs example'
-            className='header__tabs'
-          >
-            <Tab className='header__tab' label='Home' value='/' />
-            <Tab className='header__tab' label='Dogs' value='/dogs' />
-            <Tab className='header__tab' label='Cats' value='/cats' />
-            <Tab className='header__tab' label='Others' value='/others' />
-            <Tab className='header__tab' label='Contact' value='/contact' />
-          </Tabs>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  )
+ return (
+  <Box component="div" className="navbar">
+   <AppBar position="static" className="headers">
+    <Toolbar className="header">
+     <Typography variant="h6" className="header__name">
+      {APLICATION_NAME}
+     </Typography>
+     <Tabs
+      value={value}
+      onChange={handleChange}
+      aria-label="simple tabs example"
+      className="header__tabs"
+     >
+      <Tab className="header__tab" label="Home" value="/" />
+      <Tab className="header__tab" label="Dogs" value="/dogs" />
+      <Tab className="header__tab" label="Cats" value="/cats" />
+      <Tab className="header__tab" label="Others" value="/others" />
+      <Tab className="header__tab" label="Contact" value="/contact" />
+     </Tabs>
+    </Toolbar>
+   </AppBar>
+  </Box>
+ )
 }
 
 function ContactBar() {
-  return (
+ return (
+  <Grid
+   component="div"
+   className="contact"
+   container
+   justify="space-between"
+   alignItems="center"
+   fontSize={10}
+  >
+   <Grid
+    component="div"
+    className="contact__left"
+    container
+    justify="space-around"
+    item
+    xs
+    alignItems="center"
+   >
     <Grid
-      component='div'
-      className='contact'
-      container
-      justify='space-between'
-      alignItems='center'
-      fontSize={10}
+     component="span"
+     className="contact__left-address"
+     container
+     justify="flex-start"
+     item
+     xs={3}
+     alignItems="center"
     >
-      <Grid
-        component='div'
-        className='contact__left'
-        container
-        justify='space-around'
-        item
-        xs
-        alignItems='center'
-      >
-        <Grid
-          component='span'
-          className='contact__right-address'
-          container
-          justify='flex-start'
-          item
-          xs={2}
-          alignItems='center'
-        >
-          <LocationOn />
-          Address
-        </Grid>
-        <Grid
-          component='span'
-          container
-          justify='flex-start'
-          item
-          xs={3}
-          alignItems='center'
-          className='contact__right-email'
-        >
-          <Email />
-          adoption@pet.mail
-        </Grid>
-        <Grid
-          component='span'
-          className='contact__right-phone'
-          container
-          justify='flex-start'
-          item
-          xs
-          alignItems='center'
-        >
-          <Phone />
-          +506 8573-9587
-        </Grid>
-      </Grid>
-      <Grid
-        component='div'
-        className='contact__right'
-        container
-        justify='flex-end'
-        item
-        xs
-        alignItems='center'
-      >
-        <Box component='span' className='contact__right-text'>
-          Don't forget to follow us:
-        </Box>
-        <Box component='span'>
-          <FontAwesomeIcon icon={faCode} />
-        </Box>
-        <Box component='span'>Twitter</Box>
-      </Grid>
+     <LocationOn />
+     Cartago, Tres Rios
     </Grid>
-  )
+    <Grid
+     component="span"
+     container
+     justify="flex-start"
+     item
+     xs={3}
+     alignItems="center"
+     className="contact__left-email"
+    >
+     <Email />
+     adoption@pet.mail
+    </Grid>
+    <Grid
+     component="span"
+     className="contact__left-phone"
+     container
+     justify="flex-start"
+     item
+     xs
+     alignItems="center"
+    >
+     <Phone />
+     +506 8573-9587
+    </Grid>
+   </Grid>
+   <Grid
+    component="div"
+    className="contact__right"
+    container
+    justify="flex-end"
+    item
+    xs
+    alignItems="center"
+   >
+    <Grid component="span" item className="contact__right-text">
+     Follow us on:
+    </Grid>
+    <Grid item component="div" className="contact__right-icon">
+     <Facebook />
+    </Grid>
+    <Grid item component="div" className="contact__right-icon">
+     <Twitter />
+    </Grid>
+   </Grid>
+  </Grid>
+ )
 }
 
 function Header(props) {
-  const [value, setValue] = React.useState('/')
-  let { history } = props
+ const [value, setValue] = React.useState('/')
+ let { history } = props
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue)
-    console.log(newValue)
-    history.push(newValue)
-  }
+ const handleChange = (event, newValue) => {
+  setValue(newValue)
+  console.log(newValue)
+  history.push(newValue)
+ }
 
-  return (
-    <Box component='header' className='App-header'>
-      <ContactBar></ContactBar>
-      <NavBar value={value} handleChange={handleChange}></NavBar>
-    </Box>
-  )
+ return (
+  <Box component="header" className="App-header">
+   <ContactBar></ContactBar>
+   <NavBar value={value} handleChange={handleChange}></NavBar>
+  </Box>
+ )
 }
 
 export default Header
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,134 +5,169 @@ import exampleImg02 from '../../img/example-02.jpg'
 import exampleImg03 from '../../img/example-03.jpg'
 import { APLICATION_NAME } from '../../costants/AplicationCostants'
 import {
-  Grid,
-  Typography,
-  Button,
-  Card,
-  CardContent,
-  CardMedia
+ Grid,
+ Typography,
+ Button,
+ Card,
+ CardContent,
+ CardMedia,
+ Avatar,
 } from '@material-ui/core'
 
+function StartSection() {
+ return (
+  <Grid component="section" className="slide">
+   <Typography component="div">
+    <Box
+     fontSize="h2.fontSize"
+     fontWeight="fontWeightMedium"
+     className="slide__title"
+    >
+     Thinking about adopting a new friend?
+    </Box>
+    <Box fontSize="h5.fontSize" className="slide__text">
+     Try lookin at the different kind of partners you can get with our help.
+    </Box>
+   </Typography>
+   <Box className="slide__buttons">
+    <Button
+     variant="contained"
+     color="primary"
+     size="large"
+     disableElevation
+     className="slide__buttons-primary"
+    >
+     Find out how
+    </Button>
+    <Button
+     color="primary"
+     size="large"
+     disableElevation
+     className="slide__buttons-secondary"
+     variant="outlined"
+     component="span"
+    >
+     Want to put in adoption
+    </Button>
+   </Box>
+  </Grid>
+ )
+}
+
+function PetsSection() {
+ return (
+  <Typography component="section" className="pets">
+   <Grid
+    container
+    alignItems="center"
+    direction="column"
+    className="pets__title"
+   >
+    <Box fontSize="h3.fontSize" fontWeight="fontWeightMedium">
+     Our Pets
+    </Box>
+    <Box fontSize="h6.fontSize">
+     We have wonderful cats and dogs ready for adoption.
+    </Box>
+    <Box fontSize="h6.fontSize">Find you perfect match today!</Box>
+   </Grid>
+   <Grid container justify="center" className="pets__cards">
+    <Card className="card">
+     <CardMedia
+      className="card__figure"
+      image={exampleImg02}
+      title="white cat picture"
+     />
+     <CardContent>
+      <Typography gutterBottom variant="h5" component="h2">
+       Kuni
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p">
+       She's one of the babies my cat had half a year ago but i can't keep her.
+      </Typography>
+     </CardContent>
+    </Card>
+    <Card className="card">
+     <CardMedia
+      className="card__figure"
+      image={exampleImg01}
+      title="Golden Retriever puppy picture"
+     />
+     <CardContent>
+      <Typography gutterBottom variant="h5" component="h2">
+       Scott
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p">
+       We found scott in the trash with a couple of toilet paper rolls.
+      </Typography>
+     </CardContent>
+    </Card>
+    <Card className="card">
+     <CardMedia
+      className="card__figure"
+      image={exampleImg03}
+      title="dog with a stick picture"
+     />
+     <CardContent>
+      <Typography gutterBottom variant="h5" component="h2">
+       Robert
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p">
+       Robert is a very energetic boy and i don't have enough space to let him
+       play.
+      </Typography>
+     </CardContent>
+    </Card>
+   </Grid>
+  </Typography>
+ )
+}
 function HomePage() {
-  return (
-    <Grid component='div' className='home'>
-      <Grid component='section' className='slide'>
-        <Typography component='div'>
-          <Box
-            fontSize='h2.fontSize'
-            fontWeight='fontWeightMedium'
-            className='slide__title'
-          >
-            Thinking about adopting a new friend?
-          </Box>
-          <Box fontSize='h5.fontSize' className='slide__text'>
-            Try lookin at the different kind of partners you can get with our
-            help.
-          </Box>
-        </Typography>
-        <Box className='slide__buttons'>
-          <Button
-            variant='contained'
-            color='primary'
-            size='large'
-            disableElevation
-            className='slide__buttons-primary'
-          >
-            Find out how
-          </Button>
-          <Button
-            color='primary'
-            size='large'
-            disableElevation
-            className='slide__buttons-secondary'
-            variant='outlined'
-            component='span'
-          >
-            Want to put in adoption
-          </Button>
-        </Box>
-      </Grid>
-      <Typography component='section' className='pets'>
-        <Grid
-          container
-          alignItems='center'
-          direction='column'
-          className='pets__title'
-        >
-          <Box fontSize='h3.fontSize' fontWeight='fontWeightMedium'>
-            Our Pets
-          </Box>
-          <Box fontSize='h6.fontSize'>
-            We have wonderful cats and dogs ready for adoption.
-          </Box>
-          <Box fontSize='h6.fontSize'>Find you perfect match today!.</Box>
-        </Grid>
-        <Grid container justify='center' className='pets__cards'>
-          <Card className='card'>
-            <CardMedia
-              className='card__figure'
-              image={exampleImg02}
-              title='Golden Retriever puppy picture'
-            />
-            <CardContent>
-              <Typography gutterBottom variant='h5' component='h2'>
-                Kuni
-              </Typography>
-              <Typography variant='body2' color='textSecondary' component='p'>
-                She's one of the babies my cat had half a year ago but i can't
-                keep her.
-              </Typography>
-            </CardContent>
-          </Card>
-          <Card className='card'>
-            <CardMedia
-              className='card__figure'
-              image={exampleImg01}
-              title='Golden Retriever puppy picture'
-            />
-            <CardContent>
-              <Typography gutterBottom variant='h5' component='h2'>
-                Scott
-              </Typography>
-              <Typography variant='body2' color='textSecondary' component='p'>
-                We found scott in the trash with a couple of toilet paper rolls.
-              </Typography>
-            </CardContent>
-          </Card>
-          <Card className='card'>
-            <CardMedia
-              className='card__figure'
-              image={exampleImg03}
-              title='Golden Retriever puppy picture'
-            />
-            <CardContent>
-              <Typography gutterBottom variant='h5' component='h2'>
-                Robert
-              </Typography>
-              <Typography variant='body2' color='textSecondary' component='p'>
-                Robert is a very energetic boy and i don't have enough space to
-                let him play.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Typography>
-      <Typography component='section' className='team'>
-        <Grid
-          container
-          alignItems='center'
-          direction='column'
-          className='team__title'
-        >
-          <Box fontSize='h3.fontSize' fontWeight='fontWeightMedium'>
-            Meet Our team
-          </Box>
-          <Box fontSize='h6.fontSize'>
-            {APLICATION_NAME} has a team of experts that gives it support 24/7.
-          </Box>
-        </Grid>
-      </Typography>
+ return (
+  <Grid component="div" className="home">
+   <StartSection></StartSection>
+   <PetsSection></PetsSection>
+   <Typography component="section" className="team">
+    <Grid
+     container
+     alignItems="center"
+     direction="column"
+     className="team__title"
+    >
+     <Box fontSize="h3.fontSize" fontWeight="fontWeightMedium">
+      Meet Our team
+     </Box>
+     <Box fontSize="h6.fontSize">
+      {APLICATION_NAME} has a team of experts that gives it support 24/7.
+     </Box>
     </Grid>
-  )
+    <Grid container alignItems="center" className="team__pictures">
+     <Grid
+      container
+      alignItems="center"
+      direction="column"
+      className="team-pictures"
+     >
+      <Avatar
+       alt="Carlos Madrigal"
+       src=""
+       className="team-pictures__member-figure"
+      />
+      <Box
+       component="span"
+       className="pictures__member-name"
+       fontSize="h5.fontSize"
+       fontWeight="fontWeightMedium"
+      >
+       Carlos Madrigal
+      </Box>
+      <Box component="span" className="pictures__member-ocupation">
+       Application Developer
+      </Box>
+     </Grid>
+    </Grid>
+   </Typography>
+  </Grid>
+ )
 }
 export default HomePage
