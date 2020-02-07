123c329bd79d76eddb45463822343db4a0f196e3
Carlos D Madrigal <cmadrigaln@ucenfotec.ac.cr>
2020-02-06 20:29:59 -0600

    header
    Implementinmg header functionality

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import './App.scss'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Header from './components/Header'
 import HomePage from './pages/Home'
+import DogsListPage from './pages/Dogs'
 import Footer from './components/Footer'
 
-function App() {
- return (
-  <div className="App">
-   <Router>
-    <Header></Header>
-    <Switch>
-     <Route path="/" component={HomePage} />
-    </Switch>
-    <Footer></Footer>
-   </Router>
-  </div>
- )
+function App(props) {
+  return (
+    <div className='App'>
+      <Router>
+        <Header history={props.history}></Header>
+        <Switch>
+          <Route path='/' component={HomePage} />
+          <Route path='/dogs' component={DogsListPage} />
+        </Switch>
+        <Footer></Footer>
+      </Router>
+    </div>
+  )
 }
 
 export default App
