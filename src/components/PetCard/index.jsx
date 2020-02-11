85f1f18ff4ba03241560ccb188fb2314a13f6c74
Carlos David Madrigal Núñez <davidemadrigal@gmail.com>
2020-02-11 10:45:32 -0600

    petsList
    added real  info for the first four pets

--- src/components/PetCard/index.jsx
+++ src/components/PetCard/index.jsx
@@ -2,33 +2,33 @@ import React from 'react'
 import { Card, CardMedia, CardContent, Typography } from '@material-ui/core'
 
 function PetCard({ content, styles, handleCardClick }) {
-  return (
-    <Card className={styles.card} onClick={() => handleCardClick(content.id)}>
-      <CardMedia
-        className={styles.picture}
-        image={content.picture}
-        title={`${content.name} picture`}
-      />
-      <CardContent overflow='hidden'>
-        <Typography
-          gutterBottom
-          variant='h5'
-          component='h2'
-          className={styles.title}
-        >
-          {content.name}
-        </Typography>
-        <Typography
-          variant='body2'
-          component='p'
-          className={styles.info}
-          overflow='hidden'
-        >
-          {content.info}
-        </Typography>
-      </CardContent>
-    </Card>
-  )
+ return (
+  <Card className={styles.card} onClick={() => handleCardClick(content.id)}>
+   <CardMedia
+    className={styles.picture}
+    image={content.picture}
+    title={`${content.name} picture`}
+   />
+   <CardContent overflow="hidden">
+    <Typography
+     gutterBottom
+     variant="h5"
+     component="h2"
+     className={styles.title}
+    >
+     {content.name}
+    </Typography>
+    <Typography
+     variant="body2"
+     component="p"
+     className={styles.info}
+     overflow="hidden"
+    >
+     {content.info}
+    </Typography>
+   </CardContent>
+  </Card>
+ )
 }
 
 export default PetCard
